705556d80b17cc1d35685e154950f460eaf91d97
szucscsombor <szucs.abel.csombor@students.jedlik.eu>
2022-02-14 12:00:06 +0100

    retry

--- src/store/axios.instance.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const $axios = axios.create({
-  baseURL: "https://jedlik-expr-mongoose-backend.herokuapp.com/",
-  withCredentials: true,
-});
-
-export default $axios;
--- src/views/StartPageView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-  import { VContainer } from "vuetify/components";
-</script>
-
-<template>
-  <v-container class="page text-center">
-    <h1 class="text-h4">{{ $t("startPage") }}</h1>
-  </v-container>
-</template>
